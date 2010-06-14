e302215f13b0d7c6279cf1b08cee77456f7a670e
draless <draless@mailoo.org>
2010-06-14 15:30:58 +0200

    Update file

---
+++ src/algorithm.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#	algorithm.py
+#	
+#	Copyright 2010 Draless <draless@mailoo.org>
+#	
+#	This program is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU general Public License as published by
+#	the Free Software Foundation; either version 2 of the License, or
+#	(at your options) any later version.
+#	
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	Gnu General Public License for more details.
+#
+#	You should have received a copy of the Gnu General Public License
+#	along with this program; if not; write to the Free Software
+#	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#	MA 02110-1301, USA.
+
+"""
+Class and functions to search duplicates files
+"""
+
+
+import multiprocessing, os, hashlib, time, gtk
+
+
+# -----------------------
+# Cette classe est comme un processus.
+# -----------------------
+
+class Algorithm(multiprocessing.Process):
+    """
+    Search duplicates files
+    """
+    
+    def __init__(self, queue_send, path, options={}):
+        """
+        Initilize algorithm
+        
+        Arguments:
+        - `queue_send`: The queue for communicate with the parent's process
+        - `path`: The path were you want to search duplicates files
+        - `options`: Options for algorithm
+        """
+        multiprocessing.Process.__init__(self)
+        
+        self.queue_send = queue_send
+        self.path = path
+        
+        self.progress = 0.0
+        self.action = 'Initialisation'
+        self.step = 0
+        self.done = False
+
+
+
+    def update_infos(self):
+        """
+        Send infos about algorithm progress and get signal
+        """
+
+        # On envoie un dictionaire
+        infos = {}
+        infos['progress'] = self.progress
+        infos['action'] = self.action
+        infos['step'] = self.step
+        infos['done'] = self.done
+
+        self.queue_send.put(infos)
+
+        return True
+
+        
+
+    def get_all_files_paths(self, dir_path):
+        """
+        Return a list containing all file path of file in
+        a directory.
+
+        Arguments:
+        - `dir_path`: The dir path
+        """
+
+        list_files_path = []
+
+
+        # On parcour l'arbre des dossiers récursivement et on stock tout les fichiers
+        # recontrés dans une liste
+        for elem_path in self._get_listdir(dir_path):
+
+            # On contruit le chemin absolu.
+            abs_path = dir_path + '/' + elem_path
+
+            # Si c'est un fichier, on l'ajoute à la liste, si c'est un dossier,
+            # on le parcour avec cette fonction.
+            if os.path.isfile(abs_path):
+                list_files_path.append(abs_path)
+
+            elif os.path.isdir(abs_path):
+                list_files_path += self.get_all_files_paths(abs_path)
+
+        return list_files_path
+
+
+
+    def get_content(self, file_path):
+        """
+        Return Content of file @file_path
+
+        Arguments:
+        - `file_path`: The file's path
+        """
+
+        # On prend un descripteur de fichier pour pouvoir tester si le fichier
+        # est bloquant ou pas (comme par exemple '/dev/null')
+        try:
+            file_des = os.open(file_path, os.O_RDONLY|os.O_NONBLOCK)
+        except:
+            print "Impossible d'ouvrir le fichier", file_path
+            return False
+
+        # On convertit le descripteur de fichier en object file
+        file_obj = os.fdopen(file_des)
+
+        # Il se peut que l'on a le droit d'écrire dessus mais pas de le lire
+        try:
+            content = file_obj.read()
+        except:
+            print "Impossible de lire le fichier", file_path
+            file_obj.close()
+            return False
+
+        file_obj.close()
+
+        return content
+
+
+
+    def _get_listdir(self, dir_path):
+        """
+        Return a list containing all elements in the @dir_path
+
+        Arguments:
+        - `dir_path`: The dir path where you want get
+        """
+
+        try:
+            return os.listdir(dir_path)
+        except:
+            print "Impossible d'acceder au dossier", dir_path
+            return []
+
+
+
+    # -----------------------
+    # Run function
+    # -----------------------
+
+    def run(self):
+        """
+        Call when the start function is called.
+
+        Return a list of duplicate's file.
+        """
+        
+        # -----------------------
+        # On fait la liste des fichiers
+        # -----------------------
+
+        self.step = 1
+        self.action = 'Making files list'
+        self.progress = -1
+        
+        self.update_infos()
+
+        list_all_files_paths = self.get_all_files_paths(self.path)
+
+
+        # -----------------------
+        # On garde que les fichiers qui ont la meme taille
+        # -----------------------
+
+        self.step = 2
+        self.action = 'Sort files list'
+        self.progress = 0
+
+        dico_filesize = {}
+        list_len = len(list_all_files_paths)
+        i = 0
+
+        for file_path in list_all_files_paths:
+            self.update_infos()
+
+            # Calcul de la progression de la tache
+            i += 1
+            self.progress = float(i) / float(list_len)
+            
+            # On prend sa taille
+            try:
+                file_size = os.path.getsize(file_path)
+            except:
+                print "Impossible d'agir sur le fichier", file_path
+                continue
+
+            # Et on ajoute le nom du fichier associer à sa taille dans le dico
+            if not dico_filesize.has_key(file_size):
+                dico_filesize[file_size] = []
+
+            dico_filesize[file_size].append(file_path)
+
+            # On envoie les informations
+            
+            
+
+            if self.done: return
+
+            
+
+        # -----------------------
+        # On recherche les fichiers qui ont une meme somme md5
+        # -----------------------
+
+        self.action = 'Searching duplicates files'
+        self.progress = 0
+
+        
+
+        dico_md5 = {}
+        list_len = len(dico_filesize.values())
+        i = 0
+        last_p = 0
+
+        for list_file in dico_filesize.values():
+
+            i += 1
+            self.progress = float(i) / float(list_len)
+            
+            # On envoie les infos tout les 1% pour éviter la lenteur
+            if int(self.progress * 100) > last_p:
+                self.update_infos()
+                last_p = int(self.progress * 100)
+            
+
+            # Si la liste contient plus d'un item, il convient de faire une 
+            # somme md5 pour vérifier si les fichiers sont bien identiques.
+
+            if len(list_file) > 1:
+                for file_path in list_file:
+                    # On prend le contenu du fichier
+                    content = self.get_content(file_path)
+
+                    if content == False:
+                        continue
+
+                    md5_sum = hashlib.md5( content ).hexdigest()
+
+                    # Si l'entré dans le dico n'éxiste pas encore, on a créée
+                    if not dico_md5.has_key(md5_sum):
+                        dico_md5[md5_sum] = []
+
+                    dico_md5[md5_sum].append(file_path)
+
+            
+            if self.done: return
+
+
+        # On contruit la liste des doublons
+        list_dbl = []
+        for item in dico_md5.values():
+            if len(item) > 1:
+                list_dbl.append(item)
+
+        # Pour que la variable soit accéssible de l'extérieur ..
+        self.list_dbl = list_dbl
+
+        # La recherche est finie !
+        self.action = 'Finished'
+        self.progress = 1.0
+        print 'Terminated !'
+        self.done = True
+
+        self.update_infos()        
+
+        return
---
+++ src/tab_search.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#	tab_search.py
+#	
+#	Copyright 2010 Draless <draless@mailoo.org>
+#	
+#	This program is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU general Public License as published by
+#	the Free Software Foundation; either version 2 of the License, or
+#	(at your options) any later version.
+#	
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	Gnu General Public License for more details.
+#
+#	You should have received a copy of the Gnu General Public License
+#	along with this program; if not; write to the Free Software
+#	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#	MA 02110-1301, USA.
+
+"""
+The interface of a search's tab.
+"""
+
+
+import gtk, pygtk
+
+class TabSearch():
+    """
+    Function of a search's tab
+    """
+    
+    def __init__(self, title='Recherche'):
+        """
+        Initilize the tab's search
+        
+        Arguments:
+        - `title`: The tab title
+        """
+        
+        # On contruit l'onglet à partir du fichier Glade
+        # On prend juste les widgets qu'il y a dans la fenetre
+
+        self.interface = gtk.Builder()
+        self.interface.add_from_file('tab_search.glade')
+
+        self.interface.connect_signals(self)
+
+        # Le calque principale s'appelle 'main_box'
+        self.main_box = self.interface.get_object('main_box')
+        # On lui enlève son parent
+        self.main_box.unparent()
+
+        self.label_title = gtk.Label(title)
+
+
+    def set_pb(self, progress, text):
+        """
+        Set the progress bar infos
+        
+        Arguments:
+        - `progress`: The progress bar fraction
+        - `text`: The progress bar text
+        """
+        
+        pb = self.interface.get_object('pb_progress')
+
+        if progress == -1:
+            # On met la barre en mode attente
+            pb.pulse()
+        else:
+            pb.set_fraction(float(progress))
+
+        pb.set_text(text)
+
+            
+    def set_label(self, text):
+        """
+        Set the label_search_path text
+        
+        Arguments:
+        - `text`: The new text of the label
+        """
+        
+        label = self.interface.get_object('label_search_path')
+        label.set_text(text)
+
+
+    def set_title(self, title):
+        """
+        Set the new title of the tab's search
+        
+        Arguments:
+        - `title`: The new title of the tab's search
+        """
+        
+        self.label_title.set_text(title)
+        
+
